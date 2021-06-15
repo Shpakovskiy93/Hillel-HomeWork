36e472c9d5b4a2f6afcf750a8b109bc5dea8b82c
Shpakovskiy93 <mikhail.shpakovskiy.93@gmail.com>
2021-06-15 18:57:16 +0300

    hw-13

---
+++ HW-13-Shpakovskiy/js/main.js
@@ -0,0 +1,70 @@
+
+function Accordion(element) {
+    
+    this.element = element;
+    this.init = accordionInit;
+    this.toogle = toggleItem;
+    this._onItemClick = onItemClick;
+    this.addBlock = addBlock;
+    this.openAll = openAll;
+    this.closeAll = closeAll;
+    this.getOpenIndexes = getOpenIndexes;
+
+    this.element.addEventListener('click', this._onItemClick.bind(this));
+
+}
+
+function getOpenIndexes() {
+    let openIndex = [];
+    Array.prototype.forEach.call(this.element.children, e => {
+        openIndex.push(e)
+        if(e.classList.contains('extended')) {
+            console.log(openIndex.indexOf(e));
+        }
+    })
+}
+
+function openAll() {
+    Array.prototype.forEach.call(this.element.children, e => {
+        e.classList.add('extended');
+    })
+}
+function closeAll() {
+    Array.prototype.forEach.call(this.element.children, e => {
+        e.classList.remove('extended');
+    })
+}
+
+
+function accordionInit() {
+    Array.prototype.forEach.call(this.element.children, e => {
+        e.classList.add('accordion-item');
+        e.children[0].classList.add('title');
+        e.children[1].classList.add('content');
+    })
+}
+
+function toggleItem(index) {
+    this.element.children[index].classList.toggle('extended')
+}
+
+function onItemClick(e) {
+    e.target.parentElement.classList.toggle('extended');
+}
+
+
+
+function addBlock(title, description) {
+    this.element.insertAdjacentHTML('beforeEnd', `<div class="accordion-item">
+        <div class="title accordion__item-title">${title}</div>
+        <div class="content accordion__item-text">${description}</div>
+    </div>`);
+}
+
+const mainAccordion = new Accordion(document.getElementById('accordion'));
+mainAccordion.init();
+
+
+
+
+
